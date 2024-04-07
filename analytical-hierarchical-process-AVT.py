f04e3d3956ee8c0b65e7624ef87a9783f4367b01
Alberto Valiente Thoresen <46265925+albertovth@users.noreply.github.com>
2024-04-07 02:04:57 +0200

    Update analytical-hierarchical-process-AVT.py

--- analytical-hierarchical-process-AVT.py
+++ analytical-hierarchical-process-AVT.py
@@ -6,8 +6,8 @@ Created on Sat Mar 23 19:14:26 2024
 @author: albertovth
 """
 ### 
-## This Streamlit app for Analytical Hierarchy Process (AHP) was developed by Alberto Valiente Thoresen, 
-## with conceptual and troubleshooting support by OpenAI's ChatGPT 4.0. 
+## This Streamlit app for Analytical Hierarchy Process (AHP) was developed by Alberto Valiente Thoresen. 
+## Conceptual and troubleshooting support provided by OpenAI's ChatGPT 4.0. 
 ###
 
 import streamlit as st
